70db7f560c9b63040a02a532945a02d70eb6ec5a
EmmaAbe <emma.abe.bradford@gmail.com>
2021-07-07 16:37:53 -0500

    fixed problems with the orm

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
